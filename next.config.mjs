1c61e27a4216e0dc9bda944897024237b2f815b9
MajdiAbdelal <majdy.moh.hos@gmail.com>
2025-12-25 09:42:58 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,15 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "lh3.googleusercontent.com",
             },
+            {
+                protocol: "https",
+                hostname: "dangerous-darcie-bareq-4a27d84a.koyeb.app",
+                port: "8080",
+                pathname: "/api/v1/assets/**",
+            },
         ],
     },
 };
 
 export default nextConfig;
+
